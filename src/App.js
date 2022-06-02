104dd53fcd3d361dcf657700180d4a86e365aed3
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-02 12:26:46 -0400

    add MovieList and render image poster

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// Components
+import MovieList from './MovieList/MovieList';
+
 function App() {
 
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([
+    {
+      "Title": "Avengers: Endgame",
+      "Year": "2019",
+      "imdbID": "tt4154796",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+  },
+  ])
 
   return (
-    <div className="App">
-      <h1>Sanity Check</h1>
+    <div className="container-fluid movie-app">
+      <div className="row">
+        <MovieList
+          movies={movies}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/MovieList/MovieList.jsx
@@ -0,0 +1,15 @@
+
+
+const MovieList = (props) => {
+  return ( 
+    <>
+      {props.movies.map((movie, idx) => (
+        <div className="image-container d-flex justify-content-start m3">
+          <img src={movie.Poster} alt="movie poster" />
+        </div>
+      ))}
+    </>
+   );
+}
+ 
+export default MovieList;
