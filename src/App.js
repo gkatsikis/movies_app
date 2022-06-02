b71f28ad993cb6a3954161d00f5518ad9a160624
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-02 12:16:33 -0400

    add useState to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+// React
+import React, { useState, useEffect } from 'react';
 
+// CSS
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+
   return (
     <div className="App">
       <h1>Sanity Check</h1>
