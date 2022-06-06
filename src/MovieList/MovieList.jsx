633dd861b755b025672e51e157fd432fa15ec649
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-06 13:25:13 -0400

    add component to movielist

--- src/MovieList/MovieList.jsx
+++ src/MovieList/MovieList.jsx
@@ -1,11 +1,18 @@
 import './MovieList.css'
 
 const MovieList = (props) => {
+  const FavoriteComponent = props.favoriteComponent
+
   return ( 
     <>
       {props.movies.map((movie, idx) => (
         <div className="image-container d-flex justify-content-start m3">
           <img src={movie.Poster} alt="movie poster" />
+          <div 
+            onClick = {() => props.handleFavoritesClick(movie)} className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavoriteComponent />  
+          </div>
         </div>
       ))}
     </>
